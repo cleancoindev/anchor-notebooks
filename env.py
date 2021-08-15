b18c1b98e22a34dd9f5fbc18a26ffe1ee322f14f
SSen <i@ssen.name>
2021-08-15 15:32:40 +0900

    refactor: separate files

--- env.py
+++ env.py
@@ -1,69 +1,27 @@
-contract_address = {
-    "bluna": {
-        "reward": "terra1ac24j6pdxh53czqyrkr6ygphdeftg7u3958tl2",
-        "hub": "terra1fflas6wv4snv8lsda9knvq2w0cyt493r8puh2e",
-        "airdropRegistry": "terra1u5ywhlve3wugzqslqvm8ks2j0nsvrqjx0mgxpk"
-    },
-    "beth": {
-        "reward": "terra1ja3snkedk4t0zp7z3ljd064hcln8dsv5x004na"
-    },
-    "moneyMarket": {
-        "market": "terra15dwd5mj8v59wpj0wvt233mf5efdff808c5tkal",
-        "collaterals": {
-            "bLuna": {
-                "type": "bLuna",
-                "denom": "ubluna",
-                "custody": "terra1ltnkx0mv7lf2rca9f8w740ashu93ujughy4s7p",
-                "token": "terra1u0t35drzyy0mujj8rkdyzhe264uls4ug3wdp3x"
-            },
-            "bEth": {
-                "type": "bEth",
-                "denom": "ubeth",
-                "custody": "terra1j6fey5tl70k9fvrv7mea7ahfr8u2yv7l23w5e6",
-                "token": "terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l"
-            }
-        },
-        "collateralsArray": [
-            {
-                "type": "bLuna",
-                "denom": "ubluna",
-                "custody": "terra1ltnkx0mv7lf2rca9f8w740ashu93ujughy4s7p",
-                "token": "terra1u0t35drzyy0mujj8rkdyzhe264uls4ug3wdp3x"
-            },
-            {
-                "type": "bEth",
-                "denom": "ubeth",
-                "custody": "terra1j6fey5tl70k9fvrv7mea7ahfr8u2yv7l23w5e6",
-                "token": "terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l"
-            }
-        ],
-        "overseer": "terra1qljxd0y3j3gk97025qvl3lgq8ygup4gsksvaxv",
-        "oracle": "terra1p4gg3p2ue6qy2qfuxtrmgv2ec3f4jmgqtazum8",
-        "interestModel": "terra1m25aqupscdw2kw4tnq5ql6hexgr34mr76azh5x",
-        "distributionModel": "terra1u64cezah94sq3ye8y0ung28x3pxc37tv8fth7h"
-    },
-    "liquidation": {
-        "liquidationContract": "terra16vc4v9hhntswzkuunqhncs9yy30mqql3gxlqfe"
-    },
-    "anchorToken": {
-        "gov": "terra16ckeuu7c6ggu52a8se005mg5c0kd2kmuun63cu",
-        "staking": "terra19nxz35c8f7t3ghdxrxherym20tux8eccar0c3k",
-        "community": "terra17g577z0pqt6tejhceh06y3lyeudfs3v90mzduy",
-        "distributor": "terra1z7nxemcnm8kp7fs33cs7ge4wfuld307v80gypj",
-        "investorLock": "",
-        "teamLock": "",
-        "collector": "terra1hlctcrrhcl2azxzcsns467le876cfuzam6jty4"
-    },
-    "terraswap": {
-        "blunaLunaPair": "terra13e4jmcjnwrauvl2fnjdwex0exuzd8zrh5xk29v",
-        "ancUstPair": "terra1wfvczps2865j0awnurk9m04u7wdmd6qv3fdnvz"
-    },
-    "cw20": {
-        "bLuna": "terra1u0t35drzyy0mujj8rkdyzhe264uls4ug3wdp3x",
-        "bEth": "terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l",
-        "aUST": "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl",
-        "ANC": "terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc",
-        "AncUstLP": "terra1vg0qyq92ky9z9dp0j9fv5rmr2s80sg605dah6f",
-        "bLunaLunaLP": "terra1tj4pavqjqjfm0wh73sh7yy9m4uq3m2cpmgva6n"
-    }
-}
+bluna_reward_addr = "terra1ac24j6pdxh53czqyrkr6ygphdeftg7u3958tl2"
+bluna_hub_addr = "terra1fflas6wv4snv8lsda9knvq2w0cyt493r8puh2e"
+bluna_airdropRegistry_addr = "terra1u5ywhlve3wugzqslqvm8ks2j0nsvrqjx0mgxpk"
+beath_reward_addr = "terra1ja3snkedk4t0zp7z3ljd064hcln8dsv5x004na"
+bluna_custody_addr = "terra1ltnkx0mv7lf2rca9f8w740ashu93ujughy4s7p"
+bluna_token_addr = "terra1u0t35drzyy0mujj8rkdyzhe264uls4ug3wdp3x"
+beth_custody_addr = "terra1j6fey5tl70k9fvrv7mea7ahfr8u2yv7l23w5e6"
+beth_token_addr = "terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l"
+moneymarket_market_addr = "terra15dwd5mj8v59wpj0wvt233mf5efdff808c5tkal"
+moneymarket_overseer_addr = "terra1qljxd0y3j3gk97025qvl3lgq8ygup4gsksvaxv"
+moneymarket_oracle_addr = "terra1p4gg3p2ue6qy2qfuxtrmgv2ec3f4jmgqtazum8"
+moneymarket_interestModel_addr = "terra1m25aqupscdw2kw4tnq5ql6hexgr34mr76azh5x"
+moneymarket_distributionModel_addr = "terra1u64cezah94sq3ye8y0ung28x3pxc37tv8fth7h"
+liquidation_addr = "terra16vc4v9hhntswzkuunqhncs9yy30mqql3gxlqfe"
+gov_addr = "terra16ckeuu7c6ggu52a8se005mg5c0kd2kmuun63cu"
+staking_addr = "terra19nxz35c8f7t3ghdxrxherym20tux8eccar0c3k"
+community_addr = "terra17g577z0pqt6tejhceh06y3lyeudfs3v90mzduy"
+distributor_addr = "terra1z7nxemcnm8kp7fs33cs7ge4wfuld307v80gypj"
+collector_addr = "terra1hlctcrrhcl2azxzcsns467le876cfuzam6jty4"
+pair_bluna_luna_addr = "terra13e4jmcjnwrauvl2fnjdwex0exuzd8zrh5xk29v"
+pair_nac_ust_addr = "terra1wfvczps2865j0awnurk9m04u7wdmd6qv3fdnvz"
+cw20_bluna_addr = "terra1u0t35drzyy0mujj8rkdyzhe264uls4ug3wdp3x"
+cw20_beth_addr = "terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l"
+cw20_aust_addr = "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl"
+cw20_anc_addr = "terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc"
+lp_anc_ust_addr = "terra1vg0qyq92ky9z9dp0j9fv5rmr2s80sg605dah6f"
+lp_bluna_luna_addr = "terra1tj4pavqjqjfm0wh73sh7yy9m4uq3m2cpmgva6n"
